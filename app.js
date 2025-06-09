f63de294af469cda76fa1cf149c3f7ac80ab57e9
debrajdeb137 <debrajdeb137@gmail.com>
2025-06-09 13:39:19 +0530

    brand

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 const categoryRoutes = require("../backend/routes/category");
+const brandRoutes = require("../backend/routes/brand");
 app.use(cors());
 app.use(express.json())
+
+
 app.get("/",(req,res)=>{
     res.send("server running");
 })
+
+
 app.use("/category",categoryRoutes);
+app.use("/brand",brandRoutes); 
+
 async function connectDb() {
     await mongoose.connect("mongodb://localhost:27017",{
         dbName: "e-comm-store-db"
